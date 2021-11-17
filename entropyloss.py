0c58f71282f761222e20cdfe7d4e3a30bb12fdf6
Eilon Aharon <eilonah97@gmail.com>
2021-11-17 16:00:16 +0200

    Refactor: Entropy

--- entropyloss.py
+++ entropyloss.py
@@ -51,35 +51,47 @@ class EntropyLoss():
         self.Kuniq = args.Kuniq
 
     def CalcEntropyLoss(self, outputs, transActive):
-        newOutputs = torch.zeros(len(outputs), int(np.shape(outputs)[1] / (self.extraclass))).cuda()
-        if not (transActive):
+        newOutputs = torch.zeros(len(outputs), np.shape(outputs)[1] // self.extraclass).cuda()
+        if not transActive:
             mask = torch.zeros(len(outputs), int(np.shape(outputs)[1]))
             _, predicted = outputs.max(1)
-        scores = F.softmax(outputs, dim=1)
-        if not (transActive):
+
             for row in range(len(outputs)):
-
-                mask[row,int(predicted[row].item()/5)*self.extraclass:(int(predicted[row].item()/5)+1)*self.extraclass] +=1
-
+                st = (predicted[row].item() // self.extraclass) * self.extraclass
+                ed = st + self.extraclass
+                mask[row, st:ed] += 1
 
             mask = mask.to(self.device)
             sumProb = (1 / self.batch_size) * torch.sum(mask * outputs, dim=0)
+
+        scores = F.softmax(outputs, dim=1)
+
         uniformLoss = 0
         uniquenessLoss = 0
         maxEnt = 0
-        unifarr = torch.ones( int(np.shape(outputs)[1]))
+        unifarr = torch.ones(int(np.shape(outputs)[1]))
         unifarr = unifarr.to(self.device)
         k = 0
         for i in range(np.shape(newOutputs)[1]):
-            if not (transActive):
-                uniformLoss += self.Kunif * (self.criterion(sumProb[k:k + self.extraclass]))
+            if not transActive:
+                uniformLoss -= self.Kunif * (self.criterion(
+                    sumProb[i * self.extraclass:
+                            (i + 1) * self.extraclass]))
+
                 uniquenessLoss += (self.Kuniq / self.batch_size) * self.criterion(
-                    mask[:, k:k + self.extraclass] * outputs[:, k:k + self.extraclass])
-                maxEnt -= self.Kunif*(self.criterion(unifarr[k:k+self.extraclass]))
-            newOutputs[:, i] = torch.sum(scores[:, k: k + self.extraclass], dim=1)
-            k += self.extraclass
+                    mask[:, i * self.extraclass:
+                            (i + 1) * self.extraclass] * outputs[:, k:k + self.extraclass])
+
+                maxEnt -= self.Kunif * (self.criterion(
+                    unifarr[i * self.extraclass:
+                            (i + 1) * self.extraclass]))
+
+            newOutputs[:, i] = torch.sum(
+                scores[:, i * self.extraclass:
+                          (i + 1) * self.extraclass], dim=1)
+
             if self.isTrans and not (transActive) and i == 4:
                 break
         # print(uniformLoss.item())
-        print(uniformLoss*100/maxEnt)
+        print(uniformLoss * 100 / maxEnt)
         return uniquenessLoss, uniformLoss, newOutputs
